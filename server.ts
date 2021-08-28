7a62324a192e33cd77f7eab0db23ca3ab78c3e9c
achint740 <achintdawra740@gmail.com>
2021-08-28 23:02:30 +0530

    Setting up Server for Testing Purpose

--- server.ts
+++ server.ts
@@ -11,6 +11,16 @@ server.use(session({
     saveUninitialized: true,
 }));
 
+server.use((req, res, next) => {
+    res. header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
+server.get('/',(req,res)=>{
+    res.send({response : 'Success'})
+})
+
 const PORT = process.env.PORT || 6979
 server.listen(PORT,()=>{
     console.log('Server started at http://localhost:'+PORT);
